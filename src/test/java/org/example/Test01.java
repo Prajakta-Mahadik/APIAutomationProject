ecc815886bee229a2d3b2ebaf1610c61d31bcd3f
Prajakta-Mahadik <prajaktamahadik2013@gmail.com>
2024-09-17 21:02:08 +0530

    API Testing Programs

---
+++ src/test/java/org/example/Test01.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import io.restassured.RestAssured;
+
+public class Test01 {
+    public static void main(String[] args) {
+
+        System.out.println("HELLO REST ASSURED API !!!");
+    }
+}
