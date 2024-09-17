083baa144b1a2c25ddee6d6677872944c2401ba6
Prajakta-Mahadik <prajaktamahadik2013@gmail.com>
2024-09-17 22:05:18 +0530

    API Testing Programs

--- src/test/java/org/example/Test02.java
+++ src/test/java/org/example/Test02.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import io.restassured.RestAssured;
+
 public class Test02 {
     public static void main(String[] args) {
-        System.out.println("Hello!!");
+        System.out.println("REST ASSURE GET Request Demo");
+
+        // Gherkins Syntax
+//        given() - url, headers, body or payload
+//        when() - http methods - get, post, patch, put, delete
+//        then() - verify the response - expected result == actual result
+
+        RestAssured
+                .given()
+                 .baseUri("https://restful-booker.herokuapp.com")
+                 .basePath("/booking/1").log().all()
+                .when()
+                 .get()
+                .then().log().all()
+                 .statusCode(200);
     }
 }
