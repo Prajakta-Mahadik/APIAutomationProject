11b0dfe19684ba9c045fd5c4394deda7f24a492e
Prajakta-Mahadik <prajaktamahadik2013@gmail.com>
2024-09-17 22:08:02 +0530

    API Testing Programs..

--- src/test/java/org/example/Test02.java
+++ src/test/java/org/example/Test02.java
@@ -13,11 +13,11 @@ public class Test02 {
 
         RestAssured
                 .given()
-                 .baseUri("https://restful-booker.herokuapp.com")
-                 .basePath("/booking/1").log().all()
+                  .baseUri("https://restful-booker.herokuapp.com")
+                  .basePath("/booking/1").log().all()
                 .when()
-                 .get()
+                  .get()
                 .then().log().all()
-                 .statusCode(200);
+                  .statusCode(200);
     }
 }
